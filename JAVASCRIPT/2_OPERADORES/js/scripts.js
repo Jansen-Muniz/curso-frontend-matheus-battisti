6daaf6bd4b08f67a509f654e4e774829948679af
Jansen Muniz <jansen.muniz1@gmail.com>
2023-09-11 21:39:49 -0300

    Fim da seção de operadores

---
+++ JAVASCRIPT/2_OPERADORES/js/scripts.js
@@ -0,0 +1,80 @@
+// 1 - Number
+console.log(typeof 2)
+console.log(typeof 5.14)
+console.log(typeof -127)
+
+// 2 - Operações aritméticas
+console.log(2 + 4)
+console.log(10 - 5)
+console.log(20 / 2)
+console.log(2 * 2)
+
+console.log(5 + 4 * 2)
+
+// 3 - Special number
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 * 'asd')
+console.log(typeof NaN)
+
+// 4 - Strings
+console.log("Um texto")
+console.log(`Mais um texto`)
+console.log(`13`)
+
+console.log(typeof "Um texto")
+console.log(typeof `Mais um texto`)
+
+// 5 - Símbolos especiais em strings
+console.log('Testando a \n quebra de linha')
+console.log('Espaçamento \t de tab')
+
+// 6 - Concatenção
+console.log('Oi,' + ' tudo' + ' bem?')
+console.log(`Testando ` + `com ` + `crase!`)
+
+// 7 - Template strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+console.log(`Podemos testar qualquer coisa aqui ${console.log("teste")}`)
+
+// 8 - Booleano
+console.log(true)
+console.log(5 > 20)
+console.log(30 > 10)
+console.log(typeof false)
+
+// 9 - Comparações
+console.log(5 <= 5)
+console.log(5 < 5)
+console.log(10 == 10)
+console.log(10 == 9)
+console.log(10 != 9)
+
+// 10 - Idêntico
+console.log(9 == '9')
+console.log(9 === '9')
+console.log(9 != '9')
+console.log(9 !== '9')
+
+// 11 - Operadores lógicos
+console.log(true && true)
+console.log(true && false)
+console.log(5 > 2 && 2 < 10)
+console.log(5 > 2 && 'Matheus' === 1)
+console.log(5 > 2 || 'Matheus' === 1)
+console.log(5 < 2 || 5 > 100)
+console.log(!true)
+console.log(!5 > 2)
+
+// 12 - Empty values
+console.log(typeof null, typeof undefined)
+console.log(null === undefined)
+console.log(null == undefined)
+console.log(null == false)
+console.log(undefined == false)
+
+// 13 - Mudança de tipos
+console.log(5 * null)
+console.log('teste' * 'opa')
+console.log('10' + 1)
+console.log('10' - 1)
