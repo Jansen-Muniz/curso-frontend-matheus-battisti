4bbb8be58a3064aa25230d29f0a96c17fbb39183
Jansen Muniz <jansen.muniz1@gmail.com>
2023-09-13 21:52:41 -0300

    Fim da seção Arrays e objetos

--- JAVASCRIPT/5_ARRAYS_E_OBJS/js/scripts.js
+++ JAVASCRIPT/5_ARRAYS_E_OBJS/js/scripts.js
@@ -186,3 +186,153 @@ console.log(subArray3)
 const subArray4 = testeSlice.slice(2)
 
 console.log(subArray4)
+
+// 15 - foreacch
+const nums = [1, 2, 3, 4, 5]
+
+nums.forEach((numero) => {
+  console.log(`O número é ${numero}`)
+})
+
+const posts = [
+  {title: 'Primeiro post', category: 'PHP'},
+  {title: 'Segundo post', category: 'JavaScript'},
+  {title: 'Terceiro post', category: 'Python'}
+]
+
+posts.forEach(post => {
+  console.log(`Exibindo post: ${post.title} da categoria: ${post.category}`)
+})
+
+// 16 - includes 
+const brands = ['BMW', 'VW', 'Fiat']
+
+console.log(brands.includes('Fiat'))
+console.log(brands.includes('Kia'))
+
+if(brands.includes('BMW')){
+  console.log('Há carros da marca BMW!')
+}
+
+// 17 - reverse
+const reverseTest = [1, 2, 3, 4, 5]
+
+reverseTest.reverse()
+
+console.log(reverseTest)
+
+// 18 - trim
+const trimTest = ' testando \n'
+
+console.log(trimTest)
+console.log(trimTest.trim())
+
+console.log(trimTest.length)
+console.log(trimTest.trim().length)
+
+// 19 - padStart
+const testePadStart = '1'
+const newNumber = testePadStart.padStart(4, '0')
+
+console.log(testePadStart)
+console.log(newNumber)
+
+const testePadEnd = newNumber.padEnd(10, '0')
+
+console.log(testePadEnd)
+
+// 20 - split 
+const frase = 'O rato roeu a roupa do rei de Roma'
+const arrayDaFrase = frase.split(' ')
+
+console.log(arrayDaFrase)
+
+// 21 - join
+const fraseDeNovo = arrayDaFrase.join(' ')
+
+console.log(fraseDeNovo)
+
+const itensParaComprar = ['Mouse', 'Teclado', 'Monitor']
+const fraseDeCompra = `Precisamos comprar: ${itensParaComprar.join(', ')}`
+
+console.log(fraseDeCompra)
+
+// 22 - repeat
+const palavra = 'testando 1, 2, 3, '
+
+console.log(palavra.repeat(5))
+
+// 23 - rest operator 
+const somaInfinita = (...args) => {
+  let total = 0
+
+  for(let i = 0; i < args.length; i++){
+    total += args[i]
+  }
+
+  return total
+}
+
+console.log(somaInfinita(1, 2, 3))
+console.log(somaInfinita(10, 20, 100, 300, 400, 4, 25, 33))
+
+// 24 - for of
+const somaInfinita2 = (...args) => {
+  let total = 0
+
+  for (number of args) {
+    total += number  
+  }
+
+  return total
+}
+
+console.log(somaInfinita2(1, 2, 3, 4, 5, 9, 10, 8))
+
+// 25 - destructuring em objetos
+const userDetails = {
+  firstName: 'Matheus',
+  lastName: 'Battisti',
+  job: 'Programador'
+}
+
+const {firstName, lastName, job} = userDetails
+
+console.log(firstName, lastName, job)
+
+// renomear variável
+const {firstName: primeiroNome} = userDetails
+
+console.log(primeiroNome, firstName)
+
+// 26 - destructuring em arrays
+const myList = ['Avião', 'Submarino', 'Carro']
+const [veiculoA, veiculoB, veiculoC] = myList
+
+console.log(veiculoA, veiculoB, veiculoC)
+
+// 27 - JSON
+const myJson = '{"name": "Matheus", "age": 31, "skills": ["PHP", "JS", "Python"]}'
+
+console.log(myJson)
+console.log(typeof myJson)
+
+// 28 - JSON para objeto e objeto para JSON
+const myObject = JSON.parse(myJson)
+
+console.log(myObject)
+console.log(myObject.name)
+console.log(typeof myObject)
+
+//json inválido
+const badJson = '{"name": Matheus, "age": 31}'
+//const myBadJson = JSON.parse(badJson)
+
+myObject.isOpenToWork = true
+
+console.log(myObject)
+
+const myNewJson = JSON.stringify(myObject)
+
+console.log(myNewJson)
+console.log(typeof myNewJson)
