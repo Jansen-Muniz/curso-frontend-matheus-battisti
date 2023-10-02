f146e6f58dcf4634d2aeb60ef473224a465265ac
Jansen Muniz <jansen.muniz1@gmail.com>
2023-10-02 17:52:06 -0300

    Fim da seção JS moderno

--- JAVASCRIPT/15_JS_MODERNO/js/scripts.js
+++ JAVASCRIPT/15_JS_MODERNO/js/scripts.js
@@ -150,3 +150,43 @@ const otherInfos = {km: 100000, price: 49999}
 const car = {...carName, ...carBrand, ...otherInfos}
 
 console.log(car)
+
+// 8 - classes
+class Product {
+  constructor(name, price){
+    this.name = name
+    this.price = price
+  }
+
+  productWithDiscount(discount){
+    return this.price * ((100 - discount) / 100)
+  }
+}
+
+const shirt = new Product('Camisa gola V', 20)
+
+console.log(shirt)
+console.log(shirt.productWithDiscount(10))
+console.log(shirt.productWithDiscount(15))
+
+// 9 - herança
+class ProductWithAttributes extends Product {
+  constructor(name, price, colors){
+    super(name, price)
+    this.colors = colors
+  }
+
+  showColors(){
+    console.log('As cores são: ')
+    this.colors.forEach(color => {
+      console.log(color)
+    })
+  }
+}
+
+const hat = new ProductWithAttributes('Chápeu', 29.99, ['Preto', 'Amarelo', 'Verde'])
+
+console.log(hat)
+console.log(hat.name)
+
+hat.showColors()
